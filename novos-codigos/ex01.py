0fefd11b4bc0a599de264dc663ff37f13c961ac7
Thiago Dourado <thiagodf99@gmail.com>
2024-09-30 23:49:39 -0300

    Realização de exercício

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,11 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+
+# Cria uma variável que armazena uma string
+minha_string = "Olá, bem-vindo ao Python!"
+
+# Cria uma lista que armazena várias strings
+minha_lista_de_strings = ["maçã", "banana", "laranja", "uva", "pera"]
+
+# Exibindo os valores
+print(minha_string)
+print("Lista de frutas:", minha_lista_de_strings)
